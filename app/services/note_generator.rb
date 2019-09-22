6d0cf2aef79893c5b375a586b6aec02797f47e48
Toni Dezman <toni.dezman@gmail.com>
2019-09-22 08:55:06 +0200

    NoteGenerator implementation and tests

--- app/services/note_generator.rb
+++ app/services/note_generator.rb
@@ -1,6 +1,39 @@
 class NoteGenerator
 
   def self.generate_note(city:, temperature:)
+    temp_flag = get_temp_flag(temperature)
+    language  = get_language(city)
+    translations[language][temp_flag]
+  end
+
+  def self.get_language(city)
+    case city
+    when 'Berlin', 'Frankfurt', 'Hamburg', 'Köln', 'München'
+      'German'
+    else
+      'English'
+    end
+  end
+
+  def self.get_temp_flag(temperature)
+    return :hot  if temperature >= 30
+    return :warm if temperature >= 18
+    return :cold
+  end
+
+  def self.translations
+    @translations ||= {
+      "English" => {
+        cold: "It is cold outside. Do you have any warm clothes.",
+        warm: "It is ok temperature.",
+        hot:  "Drink a lot of water.",
+      },
+      "German" => {
+        cold: "Es ist kalt draußen. Hast du warme Kleidung?",
+        warm: "Es ist ok Temperatur.",
+        hot:  "Viel Wasser trinken.",
+      },
+    }
   end
 
 end
--- spec/services/note_generator_spec.rb
+++ spec/services/note_generator_spec.rb
@@ -2,4 +2,63 @@ require 'rails_helper'
 
 RSpec.describe NoteGenerator do
 
+  context "hot weather" do
+    it "returns note for German speaking destination" do
+      note = NoteGenerator.generate_note(city: 'Berlin',    temperature: 30)
+      expect(note).to eq('Viel Wasser trinken.')
+      note = NoteGenerator.generate_note(city: 'Frankfurt', temperature: 35)
+      expect(note).to eq('Viel Wasser trinken.')
+      note = NoteGenerator.generate_note(city: 'Hamburg',   temperature: 40)
+      expect(note).to eq('Viel Wasser trinken.')
+      note = NoteGenerator.generate_note(city: 'Köln',      temperature: 45)
+      expect(note).to eq('Viel Wasser trinken.')
+      note = NoteGenerator.generate_note(city: 'München',   temperature: 32)
+      expect(note).to eq('Viel Wasser trinken.')
+    end
+
+    it "returns note for English speaking destination" do
+      note = NoteGenerator.generate_note(city: 'London', temperature: 33)
+      expect(note).to eq('Drink a lot of water.')
+    end
+
+    it "returns note for UNKNOWN speaking destination" do
+      note = NoteGenerator.generate_note(city: 'Ljubljana', temperature: 37)
+      expect(note).to eq('Drink a lot of water.')
+    end
+  end
+
+  context "warm weather" do
+    it "returns note for German speaking destination" do
+      note = NoteGenerator.generate_note(city: 'Berlin',    temperature: 22)
+      expect(note).to eq('Es ist ok Temperatur.')
+    end
+
+    it "returns note for English speaking destination" do
+      note = NoteGenerator.generate_note(city: 'London', temperature: 18)
+      expect(note).to eq('It is ok temperature.')
+    end
+
+    it "returns note for other speaking destination" do
+      note = NoteGenerator.generate_note(city: 'Ljubljana', temperature: 21)
+      expect(note).to eq('It is ok temperature.')
+    end
+  end
+
+  context "cold weather" do
+    it "returns note for German speaking destination" do
+      note = NoteGenerator.generate_note(city: 'Berlin',    temperature: -5)
+      expect(note).to eq('Es ist kalt draußen. Hast du warme Kleidung?')
+    end
+
+    it "returns note for English speaking destination" do
+      note = NoteGenerator.generate_note(city: 'London', temperature: 8)
+      expect(note).to eq('It is cold outside. Do you have any warm clothes.')
+    end
+
+    it "returns note for other speaking destination" do
+      note = NoteGenerator.generate_note(city: 'Ljubljana', temperature: -7)
+      expect(note).to eq('It is cold outside. Do you have any warm clothes.')
+    end
+  end
+
 end
